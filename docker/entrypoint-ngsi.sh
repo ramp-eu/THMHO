d525ed125912e86af522684673de71f94b260ffe
ipa-rar <ragesh.ramachandran@ipa.fraunhofer.de>
2022-02-08 16:53:18 +0100

    added github action workflow

--- docker/entrypoint-ngsi.sh
+++ docker/entrypoint-ngsi.sh
@@ -3,6 +3,4 @@ source "/opt/ros/noetic/setup.bash" && source "$CATKIN_WS/devel/setup.bash"
 export ROS_MASTER_URI=http://192.168.0.54:11311
 export ROS_IP="$(hostname -I | cut -f1 -d' ')"
 
-#roslaunch heatmap_gen heatmap_generator.launch 
-roslaunch thmho_ngsi_converter_bringup ngsi_bringup.launch
-#rosrun thmho_ngsi_converter_bringup heatmap_to_ngsi.py
+roslaunch thmho_ngsi_converter_bringup save_costmap.launch
