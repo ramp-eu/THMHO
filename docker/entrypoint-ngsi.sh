6916c515dc265ed0473ab515bcde60cd8325eb27
ipa-rar <ragesh.ramachandran@ipa.fraunhofer.de>
2022-02-22 16:51:18 +0100

    updated docker compose

--- docker/entrypoint-ngsi.sh
+++ docker/entrypoint-ngsi.sh
@@ -3,4 +3,5 @@ source "/opt/ros/noetic/setup.bash" && source "$CATKIN_WS/devel/setup.bash"
 export ROS_MASTER_URI=http://192.168.0.54:11311
 export ROS_IP="$(hostname -I | cut -f1 -d' ')"
 
-roslaunch thmho_ngsi_converter_bringup save_costmap.launch
+roslaunch thmho_ngsi_converter_bringup ngsi_bringup.launch
+rosrun thmho_ngsi_converter_bringup costmap_saver.py
