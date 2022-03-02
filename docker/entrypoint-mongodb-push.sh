5828322eaa8d7bf09a3e7a44f749575a608fc171
ipa-rar <ragesh.ramachandran@ipa.fraunhofer.de>
2022-03-02 13:42:35 +0100

    added docker for mongodb data dump

--- docker/entrypoint-mongodb-push.sh
+++ docker/entrypoint-mongodb-push.sh
@@ -3,4 +3,4 @@ source "/opt/ros/noetic/setup.bash" && source "$CATKIN_WS/devel/setup.bash"
 export ROS_MASTER_URI=http://192.168.0.54:11311
 export ROS_IP="$(hostname -I | cut -f1 -d' ')"
 
-roslaunch thmho_ngsi_converter_bringup ngsi_bringup.launch #push to orion
+rosrun thmho_ngsi_converter_bringup costmap_saver.py # push to mongodb inhouse
