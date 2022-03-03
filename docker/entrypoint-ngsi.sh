dd51fe2432ab0d9f38312c778b1f6a4d7cada766
ipa-rar <ragesh.ramachandran@ipa.fraunhofer.de>
2022-03-03 16:36:23 +0100

    updated docker entry points

--- docker/entrypoint-ngsi.sh
+++ docker/entrypoint-ngsi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source "/opt/ros/noetic/setup.bash" && source "$CATKIN_WS/devel/setup.bash"
-export ROS_MASTER_URI=http://192.168.0.54:11311
+export ROS_MASTER_URI=http://localhost:11311
 export ROS_IP="$(hostname -I | cut -f1 -d' ')"
 
 roslaunch thmho_ngsi_converter_bringup ngsi_bringup.launch #push to orion
--- docker/entrypoint-server.sh
+++ docker/entrypoint-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source "/opt/ros/noetic/setup.bash" && source "$CATKIN_WS/devel/setup.bash"
-export ROS_MASTER_URI=http://192.168.0.54:11311
+export ROS_MASTER_URI=http://localhost:11311
 export ROS_IP="$(hostname -I | cut -f1 -d' ')"
 
 roslaunch heatmap_gen heatmap_generator.launch  #combine scans
